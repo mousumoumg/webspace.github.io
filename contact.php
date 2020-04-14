fc8f4908ee20eb1ca7b29f4b4532ca9f1910dd6d
mousumoumg <63541776+mousumoumg@users.noreply.github.com>
2020-04-14 18:41:05 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,24 +1,20 @@
 <?php
 
-<$name =$_REQUEST['name'];
-<$email =$_REQUEST['myEmail'];
-<$Mobile =$_REQUEST['myMobile No'];
-<$about =$_REQUEST['myText'];
-
-//check fied is filled or not
-
-if(empty($name) || empty($email) || empty($Mobile) || empty($about))
-
+if(isset($_REQUEST['submit']))
 {
+  <$name =$_REQUEST['name'];
+  <$email =$_REQUEST['myEmail'];
+  <$Mobile =$_REQUEST['myMobile No'];
+  <$about =$_REQUEST['myText'];
 
-    echo "Please filled all the fields";
-}
-else 
-{
-    mail("mousummoumg@gmail.com"," Web tech" ,$about, "from :$name<$email>");
+  $mailTo="mousummoumg@yahoo.in";
+  $headers="From :".$mailFrom;
+  $txt="You have received an email from".$name."./n/n".$about;
+    
+  mail($mailTo,$about,$txt,$headers);
+  header("Location: index.php?mailsend");
 
-    echo"<script type='text/javascript'>alert('Your message sent successfully');
-    </script>";
 }
 
 ?>
+
